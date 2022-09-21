a4b8be7fa86657d83ccaa0d506baa78110227a00
karen okasaki <oks.karen@gmail.com>
2022-09-21 14:57:56 -0300

    users route

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -14,10 +14,10 @@ const UserModel = require("../models/User.model");
 
 //4º rota: Criar várias receitas de uma só vez
 
-//5º rota: Deletar uma receita pelo seu ID
-
 //6º rota: Acessar todos os usuários que favoritaram essa receita
 
 //7º rota: Acessar todos os usuários que deram dislike essa receita
+
+//!5º rota: Deletar uma receita pelo seu ID - retira-la da array de favorites e dislikes dos USERS
 
 //Não se esqueça de exportar o router!
---
+++ routes/users.routes.js
@@ -0,0 +1,16 @@
+
+
+
+//1º rota: Criar um user
+
+//2º rota: Pegar todos os users
+
+//3º rota: Acessar um usuário pelo seu ID
+
+//4º Adicionar uma receita na array de favorites
+
+//5º Adicionar uma receita na array de deslikes
+
+//6º Remover uma receita na array de favorite
+
+//7º Remover uma receita na array de deslikes
